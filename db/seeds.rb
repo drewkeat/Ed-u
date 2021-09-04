876d319cc66749033540936157ba88be881435bf
Drew Keat <thedrewkeat@gmail.com>
2021-09-03 22:12:23 -0600

    reseed and reformat _self course cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,94 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(
-    name: "Administrator",
-    email: "admin@email.com",
-    password: "password",
-    access: "admin"
-)
+def create_users
+    @admin1 = User.create(
+        name: "Administrator1",
+        email: "admin1@email.com",
+        password: "password",
+        access: "admin"
+    )
 
-User.create(
-    name: "Facilitator",
-    email: "facilitator@email.com",
-    password: "password",
-    access: "facilitator"
-)
+    @admin2 = User.create(
+        name: "Administrator2",
+        email: "admin2@email.com",
+        password: "password",
+        access: "admin"
+    )
 
-User.create(
-    name: "Learner",
-    email: "learner@email.com",
-    password: "password",
-    access: "learner"
-)
+    @admin3 = User.create(
+        name: "Administrator3",
+        email: "admin3@email.com",
+        password: "password",
+        access: "admin"
+    )
+
+    @f1 = User.create(
+        name: "Facilitator1",
+        email: "facilitator1@email.com",
+        password: "password",
+        access: "facilitator"
+    )
+
+    @f2 = User.create(
+        name: "Facilitator2",
+        email: "facilitator2@email.com",
+        password: "password",
+        access: "facilitator"
+    )
+
+   @f3 = User.create(
+        name: "Facilitator3",
+        email: "facilitator3@email.com",
+        password: "password",
+        access: "facilitator"
+    )
+
+    @l1 = User.create(
+        name: "Learner1",
+        email: "learner1@email.com",
+        password: "password",
+        access: "learner"
+    )
+
+    @l2 = User.create(
+        name: "Learner2",
+        email: "learner2@email.com",
+        password: "password",
+        access: "learner"
+    )
+
+    @l3 = User.create(
+        name: "Learner3",
+        email: "learner3@email.com",
+        password: "password",
+        access: "learner"
+    )
+end
+
+def create_courses
+
+    @c1 = Course.create(name: "Course One", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, amet dolor! Voluptatem nesciunt expedita, numquam veniam est, molestiae aut nihil dolor, quaerat magni minima earum natus asperiores eius! Dolore, praesentium.")
+    
+    @c2 = Course.create(name: "Course Two", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, amet dolor! Voluptatem nesciunt expedita, numquam veniam est, molestiae aut nihil dolor, quaerat magni minima earum natus asperiores eius! Dolore, praesentium.")
+    
+    @c3 = Course.create(name: "Course Three", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, amet dolor! Voluptatem nesciunt expedita, numquam veniam est, molestiae aut nihil dolor, quaerat magni minima earum natus asperiores eius! Dolore, praesentium.")
+
+    @f1.courses << @c1
+    @f2.courses << @c2
+    @f3.courses << @c3
+
+    @f1.save
+    @f2.save
+    @f3.save
+end
+
+def enroll_learners
+    @l1.enrollments << [@c1, @c2]
+    @l2.enrollments << [@c2, @c3]
+    @l3.enrollments << [@c1, @c3]
+end
+
+create_users
+create_courses
+enroll_learners
