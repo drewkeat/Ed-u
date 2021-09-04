44804c8a03bc38f08d9b94fff716e0ba366d96e8
Drew Keat <thedrewkeat@gmail.com>
2021-09-04 12:10:21 -0600

    define user access methods in application_helper, refactor logic in users _form for admin access

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+    def admin?
+        !!current_user.access == "admin"
+    end
+
+    def facilitator?
+        !!current_user.access == "facilitator"
+    end
+
+    def learner?
+        !!current_user.access == "learner"
+    end
+
 end
