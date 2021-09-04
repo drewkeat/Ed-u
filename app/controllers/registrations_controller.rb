085e41c11428f21163a11086290990a5dff2f933
Drew Keat <thedrewkeat@gmail.com>
2021-09-03 20:23:35 -0600

    add Registration validation for uniqueness of object

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,19 @@
 class RegistrationsController < ApplicationController
     def create
         @registration = Registration.new(registration_params)
-        @course = Course.find(params[:registration][:course_id])
-        if @registration.save
-          flash[:success] = "You have been registered for #{@course.name}"
-          redirect_to user_path(current_user)
+        if @registration.duplicate?
+          flash[:warning] = "You've already registered for this course"
+          redirect_to courses_path
         else
-          flash[:danger] = "Something went wrong"
-          render courses_path
+          if @registration.save
+            flash[:success] = "You have been registered for #{@registration.course.name}"
+            redirect_to user_path(current_user)
+          else
+            flash[:danger] = "Something went wrong"
+            render courses_path
+          end
         end
-    end
+      end
 
     def destroy
         @registration = Registration.find(params[:registration_id])
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,4 +1,9 @@
 class Registration < ApplicationRecord
   belongs_to :course
   belongs_to :learner, class_name: "User"
+  validates :course_id, uniqueness: {scope: :learner_id}
+
+  def duplicate?
+    !!Registration.find_by(course_id: self.course_id ,learner_id: self.learner_id)
+  end
 end
