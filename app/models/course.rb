2ebfa904267ef7221ad1d01a34106d094cd64f5e
Drew Keat <thedrewkeat@gmail.com>
2021-09-04 21:28:22 -0600

    apply upcoming and past scopes to course class, update users _self view to only allow reviews for past courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,6 @@ class Course < ApplicationRecord
     has_many :registrations
     has_many :learners, through: :registrations, source: :learner
     has_many :reviews, as: :reviewable
+    scope :upcoming, -> {where('start_time >= ?', Time.now).order(:start_time)}
+    scope :past, -> {where('start_time <= ?', Time.now)}
 end
