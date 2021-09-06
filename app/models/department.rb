9dbbdc94301a78c9150c066b414c8ede6d31b413
Drew Keat <thedrewkeat@gmail.com>
2021-09-06 16:03:25 -0600

    Create Departments index view and add courses relationship to departments in model

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,6 @@
 class Department < ApplicationRecord
     has_many :staff, class_name: "User", foreign_key: "department_id", dependent: :nullify
+    has_many :courses, through: :staff, source: :courses
     #Validations
     validates :name, uniqueness: true
 end
