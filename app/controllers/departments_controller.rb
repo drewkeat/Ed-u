d8e306e2964f84c50db003d90b777b28471c75fc
Drew Keat <thedrewkeat@gmail.com>
2021-09-06 14:46:54 -0600

    create namespaced routes admin/departments

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,2 +1,48 @@
 class DepartmentsController < ApplicationController
+    def index
+        @departments = Department.all
+    end
+
+    def new
+        @department = Department.new
+    end
+    
+    def create
+        @department = Department.new(department_params)
+        if @department.save
+          flash[:success] = "Department successfully created"
+          redirect_to @department
+        else
+          flash[:danger] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    def edit
+        @department = department.find(params[:id])
+    end
+    
+    def update
+        @department = Department.find(params[:id])
+        if @department.update_attributes(params[:department])
+          flash[:success] = "Department was successfully updated"
+          redirect_to @department
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+    
+    def destroy
+        @department = Depratment.find(params[:id])
+        if @department.destroy
+            flash[:success] = 'Depratment was successfully deleted.'
+            redirect_to departments_url
+        else
+            flash[:error] = 'Something went wrong'
+            redirect_to departments_url
+        end
+    end
+    
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
   root to: 'sessions#home'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :reviews
+  resources :users do
+    resources :reviews 
+  end
   resources :registrations, only: [:create, :destroy]
   resources :courses do
     resources :reviews
   end
-  resources :departments, only: [:index, :show]
-  resources :users do
-    resources :reviews 
+  resources :reviews
+  scope '/admin' do
+    resources :departments
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
